69f5fb6fb52a5e0706fa350609b21f991fe0852d
Cauan Barbaglio <127343662+Cauanvmb@users.noreply.github.com>
2023-11-10 20:44:05 -0300

    Atualizado

--- src/test/java/Estoque.java
+++ src/test/java/Estoque.java
@@ -1,83 +1,82 @@
-
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
 public class Estoque {
-            ArrayList<Console> estoqueConsoles = new ArrayList<Console>();
-            Scanner sc = new Scanner(System.in);
-            int numero;
-            public void EstoqueEmUso() {
-                do {
-                System.out.printf("%nA operação que deseja fazer: %n%nSe quiser ver apenas imprimir o estoque insira 0%n%nSe deseja adicionar console(s) ao estoque insira 1%n%nSe deseja remover um item da lista insira 2%n%n");
-                numero = sc.nextInt();
-                if (numero == 1) {
-                    adicionar();
-                }
-                else if(numero == 0) {
-                    verLista();
-                }
-                else if(numero==2) {
-                    removerPorId();
-                }
-                } while(numero > -1 && numero < 3);
+    ArrayList<Console> estoqueConsoles = new ArrayList<>();
+    private Scanner sc = new Scanner(System.in);
+
+    public void estoqueEmUso() {
+        int numero;
+
+            do {
+            System.out.printf("%nA operação que deseja fazer: %n%nSe quiser ver apenas imprimir o estoque insira 0%n%nSe deseja adicionar console(s) ao estoque insira 1%n%nSe deseja remover um item da lista insira 2%n%n");
+
+            numero = sc.nextInt();
+
+            if (numero == 1) {
+                adicionar();
+            } else if (numero == 0) {
+                verLista();
+            } else if (numero == 2) {
+            System.out.print("Insira o ID do item a ser removido: ");
+            int id = sc.nextInt();
+            removerPorId(id);
             }
-        public void adicionar() {   
-                    System.out.print("Insira quantas quer inserir no estoque: ");
-                    int adicionando = sc.nextInt();
-                    for (int i = 0; i<adicionando;i+=1) {
-                    sc.nextLine();
-                    System.out.print("Insira o id do console: ");
-                    int id = sc.nextInt();
-                    sc.nextLine();
-                    System.out.print("Insira o nome do console: ");
-                    String nome = sc.nextLine();
-                    System.out.print("Insira o marca do console: ");
-                    String marca = sc.nextLine();
-                    System.out.print("Insira o modelo do console: ");
-                    String modelo = sc.nextLine();
-                    System.out.print("Insira o hd(gigas de memória) do console: ");
-                    double hd = sc.nextDouble();
-                    sc.nextLine();
-                    System.out.print("Insira a cor do console: ");
-                    String cor = sc.nextLine();
-                    Especificacao espec = new Especificacao(marca, modelo, hd, cor, id);
-                    Console item = new Console(nome,espec);
-                    estoqueConsoles.add(item);
-                    System.out.printf("%n%n%n");
-                    System.out.println(estoqueConsoles);
-                }
+            } while (numero > -1 && numero < 3);
+                throw new IllegalArgumentException("inseriu um item inexistente");
+            }
+
+    public void removerPorId(int id) {
+            Iterator<Console> iterator = estoqueConsoles.iterator();
+        while (iterator.hasNext()) {
+            Console console = iterator.next();
+            if (console.getEspec().getId() == id) {
+                iterator.remove();
+                System.out.println("Item removido com sucesso.");
+                System.out.println(estoqueConsoles);
+                return;
+            }
+            else{
+                throw new IllegalArgumentException("inseriu um item inexistente");
+            }
+        }
+
+        System.out.println("Nenhum item com o ID fornecido foi encontrado.");
+        System.out.println(estoqueConsoles);
+    }
+
+    public void adicionar() {
+        System.out.print("Insira quantos quer inserir no estoque: ");
+        int adicionando = sc.nextInt();
+
+        for (int i = 0; i < adicionando; i += 1) {
+            sc.nextLine();
+            System.out.print("Insira o id do console: ");
+            int id = sc.nextInt();
+            sc.nextLine();
+            System.out.print("Insira o nome do console: ");
+            String nome = sc.nextLine();
+            System.out.print("Insira o marca do console: ");
+            String marca = sc.nextLine();
+            System.out.print("Insira o modelo do console: ");
+            String modelo = sc.nextLine();
+            System.out.print("Insira o hd(gigas de memória) do console: ");
+            double hd = sc.nextDouble();
+            sc.nextLine();
+            System.out.print("Insira a cor do console: ");
+            String cor = sc.nextLine();
+
+            Especificacao espec = new Especificacao(marca, modelo, hd, cor, id);
+            Console item = new Console(nome, espec);
+            estoqueConsoles.add(item);
+            System.out.printf("%n%n%n");
+            System.out.println(estoqueConsoles);
+            }
         }
         public void verLista() {
             System.out.printf("%n%nAqui está o conteúdo do Estoque.%n%n");
-            System.out.println(estoqueConsoles);
-        }
-        public void removerPorId() {
-            Scanner sc = new Scanner(System.in);
-            int id;
-            while (true) {
-                try {
-                    System.out.print("Insira o ID do item a ser removido: ");
-                    id = sc.nextInt();
-                    break;
-                } catch (java.util.InputMismatchException e) {
-                    System.out.println("Entrada inválida. Por favor, insira um número válido.");
-                    sc.nextLine();
-                    sc.close();
-                }
+            for (Console console : estoqueConsoles) {
+                System.out.println(console);
             }
-            Iterator<Console> iterator = estoqueConsoles.iterator();
-            while (iterator.hasNext()) {
-                Console console = iterator.next();
-                if (console.getEspec().getId() == id) {
-                    iterator.remove();
-                    System.out.println("Item removido com sucesso.");
-                    System.out.println(estoqueConsoles);
-                    return;  
-                }
-            }
-            System.out.println("Nenhum item com o ID fornecido foi encontrado.");
-            System.out.println(estoqueConsoles);
-            sc.close();
         }
 }
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,16 +1,38 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.PrintStream;
 
 import static org.junit.Assert.*;
 
 public class Main {
 
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final InputStream originalSystemIn = System.in;
+    private final PrintStream originalSystemOut = System.out;
+    private final PrintStream originalSystemErr = System.err;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setIn(originalSystemIn);
+        System.setOut(originalSystemOut);
+        System.setErr(originalSystemErr);
+    }
+
     @Test
     public void testAdicionar() {
         String input = "1\n1\nConsole1\nMarca1\nModelo1\n500\nCor1\n";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
 
         Estoque estoque = new Estoque();
         estoque.adicionar();
@@ -30,16 +52,33 @@ public class Main {
     @Test
     public void testRemoverPorId() {
         String input = "1\n1\nConsole1\nMarca1\nModelo1\n500\nCor1\n2\n1\n";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
 
         Estoque estoque = new Estoque();
         Console console1 = new Console("Console1", new Especificacao("Marca1", "Modelo1", 500, "Cor1", 1));
         Console console2 = new Console("Console2", new Especificacao("Marca2", "Modelo2", 1000, "Cor2", 2));
         estoque.estoqueConsoles.add(console1);
         estoque.estoqueConsoles.add(console2);
-        estoque.removerPorId();
+        estoque.removerPorId(1);
         assertEquals(1, estoque.estoqueConsoles.size());
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testRemoverConsoleInexistente() {
+        Estoque estoque = new Estoque();
+        Console console1 = new Console("Console1", new Especificacao("Marca1", "Modelo1", 500, "Cor1", 1));
+        estoque.estoqueConsoles.add(console1);
+
+        estoque.removerPorId(8);
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInserirFuncaoInvalidaNoEstoqueEmUso(){
+        String input = "7\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        Estoque estoque = new Estoque();
+        estoque.estoqueEmUso();
+
+    }
 }
